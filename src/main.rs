36e8502bfd542d1c0d92599eb7315cb377ed504e
Nicolas Zordan <nicolas.zordan.tek@gmail.com>
2020-12-02 12:20:23 +0100

    added actual command line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 use chip8::emulator::Emulator;
-use std::env;
-
+use clap::{App, Arg};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args_matches = App::new("chip8_emulator")
+        .arg(
+            Arg::with_name("ROM")
+                .required(true)
+                .index(1)
+                .help("rom file to launch"),
+        )
+        .get_matches();
 
+    let rom_file = args_matches.value_of("ROM").unwrap();
     let mut chip8 = Emulator::new(20);
-    chip8
-        .load_game(&args[1])
-        .expect("failed to load game");
-
+    chip8.load_game(rom_file).expect("failed to load game");
     chip8.run();
 }
